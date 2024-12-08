bf3030c3e4bd9e6f1c2da4d9acb7cbae86d77d31
Deepti Mahisare <deeptimahisare@gmail.com>
2024-12-08 22:17:00 +0530

    crud new spring starter project created only

---
+++ SB_Crud-1/src/main/java/com/mycompany/SbCrud1Application.java
@@ -0,0 +1,13 @@
+package com.mycompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbCrud1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbCrud1Application.class, args);
+	}
+
+}
