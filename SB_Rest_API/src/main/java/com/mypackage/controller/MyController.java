cd0a3a0a5a1a372504d8b680e7a818fa1bfc5a24
Deepti Mahisare <deeptimahisare@gmail.com>
2024-12-13 14:36:35 +0530

    Spring Boot Rest API get mapping with database done

--- SB_Rest_API/src/main/java/com/mypackage/controller/MyController.java
+++ SB_Rest_API/src/main/java/com/mypackage/controller/MyController.java
@@ -1,5 +1,30 @@
 package com.mypackage.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mypackage.entity.User;
+import com.mypackage.services.AllServices;
+
+@RestController
 public class MyController {
 
+	@Autowired
+	AllServices allServices;
+	
+//	@GetMapping("/getAll")
+//	public List<User> getAll(){
+//		
+//		return allServices.getAll();
+//	}
+	
+	@GetMapping("/getAll")
+	public Iterable<User> getAll(){
+		
+		return allServices.getAll();
+	}
+	
 }
--- SB_Rest_API/src/main/java/com/mypackage/services/AllServices.java
+++ SB_Rest_API/src/main/java/com/mypackage/services/AllServices.java
@@ -1,5 +1,64 @@
 package com.mypackage.services;
 
-public class AllServices {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.mypackage.dao.UserRepository;
+import com.mypackage.entity.User;
+
+@Service
+public class AllServices {
+	
+//	--------------Manual input in API --------------------------
+	
+	
+//	List<User> li = new ArrayList<User>();
+//	
+//	public AllServices() {
+//		User user1= new User();
+//		user1.setUid(1);
+//		user1.setUname("Deep");
+//		user1.setAge(20);
+//		
+//		User user2= new User();
+//		user2.setUid(2);
+//		user2.setUname("Yogi");
+//		user2.setAge(22);
+//		
+//		User user3= new User();
+//		user3.setUid(3);
+//		user3.setUname("Nano");
+//		user3.setAge(21);
+//		
+//		
+////		----------------x----------------x------------x--------------x------------
+//		
+//		li.add(user1);
+//		li.add(user2);
+//		li.add(user3);
+//		
+//	
+//	}
+////		----------------x----------------x------------x--------------x------------
+//	public List<User> getAll(){
+//		return li;
+//	}
+
+	@Autowired
+	UserRepository userRepository;
+	
+	@GetMapping("/getAll")
+	public Iterable<User> getAll(){
+		Iterable<User> itr = userRepository.findAll();
+		return itr;
+	}
+	
+	
+	
+	
 }
