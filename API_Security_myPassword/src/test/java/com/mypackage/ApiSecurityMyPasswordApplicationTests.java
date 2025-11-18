d28279b09bcc0dffec19f62171706a0a1a8b1ae1
Deepti Mahisare <115733808+deeptimahisare1206@users.noreply.github.com>
2025-11-18 22:05:55 +0530

    Update ApiSecurityMyPasswordApplicationTests.java

--- API_Security_myPassword/src/test/java/com/mypackage/ApiSecurityMyPasswordApplicationTests.java
+++ API_Security_myPassword/src/test/java/com/mypackage/ApiSecurityMyPasswordApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ApiSecurityMyPasswordApplicationTests {
-
+//Write test cases here to test it on the runtime.
 	@Test
 	void contextLoads() {
 	}
